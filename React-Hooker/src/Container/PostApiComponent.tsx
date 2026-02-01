b23545a87224b27f5410d5c9c44b1ceb49bf4629
thatsathishkumar <sathishkumarmani2002@gmail.com>
2026-02-01 17:59:43 +0530

    new change2

--- React-Hooker/src/Container/PostApiComponent.tsx
+++ React-Hooker/src/Container/PostApiComponent.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import computer from "../../public/images/PostApiGame/computer.png";
 import server from "../../public/images/PostApiGame/server.png";
@@ -8,6 +8,7 @@ import mailData from "../../public/images/PostApiGame/mailData.png";
 import "../styles/PostApi.css";
 
 const PostApiComponent = (): React.JSX.Element => {
+  const [submiter, setsubmiter] = useState(false);
   const navigator = useNavigate();
   return (
     <div>
@@ -19,12 +20,25 @@ const PostApiComponent = (): React.JSX.Element => {
               <img
                 src={dataPack}
                 alt="dataPack"
-                className="rightPipestop"
+                className={submiter ? "rightPipe" : "rightPipestop"}
               ></img>
-              <img src={mailData} alt="mailData" className="leftPipestop"></img>
+              <img
+                src={mailData}
+                alt="mailData"
+                className={submiter ? "leftPipe" : "leftPipestop"}
+              ></img>
             </div>
             <img src={server} alt="server" className="child server"></img>
           </div>
+        </div>
+        <div>
+          <button
+            onClick={() => {
+              setsubmiter(!submiter);
+            }}
+          >
+            Submit data
+          </button>
         </div>
         <div className="navdiv">
           <button
